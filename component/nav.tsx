072769707ab4af5234f464529e22b8aeedc6e91a
Armboy122 <104450797+Armboy122@users.noreply.github.com>
2024-06-11 01:06:37 +0700

    เปลี่ยน navbar

--- component/nav.tsx
+++ component/nav.tsx
@@ -2,8 +2,34 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav>
-      navbar
+    <nav className=" bg-black">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shink-0">
+              <a href="/" className=" text-white">
+                FAST TEXT
+              </a>
+            </div>
+          </div>
+          <div className=" hidden md:block">
+            <div className=" ml-4 flex items-start space-x-4">
+              <a href="/" className=" bg-white text-black rounded-lg p-2">
+                HOME
+              </a>
+              <a href="/" className=" bg-white text-black rounded-lg p-2">
+                สวัสดิการ
+              </a>
+              <a href="/" className=" bg-white text-black rounded-lg p-2">
+                จัดซื้อจัดจ้าง
+              </a>
+              <a href="/" className=" bg-white text-black rounded-lg p-2">
+                จัดซื้อจัดจ้างเกิน 1 แสน
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
