04ab2cbde9ce27d5e6ca153fd7e0f89df38c54ff
Armboy122 <104450797+Armboy122@users.noreply.github.com>
2024-06-11 12:58:14 +0700

    แก้บัคสี

--- component/nav.tsx
+++ component/nav.tsx
@@ -1,8 +1,9 @@
+"use client"
 import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className=" bg-black">
+    <nav className=" bg-orange-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
