4b9118119ae48b4de5965700df5a2ea6b715b29d
patna <p.wiriyar@gmail.com>
2024-06-12 16:26:30 +0700

    เพิ่มข้อมูลหน้าแรก

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      หน้าแรก
-    </main>
-  );
+import prisma from "@/lib/prisma"
+import Link from "next/link";
+
+
+export default async function Home(){
+    const documents = await prisma.document.findMany({
+        select: {
+            id: true,
+            user: true,
+            name: true,
+            amount: true,
+            status: true,
+            nextStatus: true
+        }
+    })
+    const documentsWithLatestStatus = documents.map(doc => {
+        const latestStatus = doc.status.reduce((latest, current) => {
+          return new Date(current.date) > new Date(latest.date) ? current : latest;
+        }, doc.status[0]);
+        
+        return {
+          ...doc,
+          status: latestStatus,
+        };
+    });
+    return (
+        <div>
+            หน้าแรก
+            {documentsWithLatestStatus.map((val)=>{
+                return (
+                    <div key={val.id} className="flex flex-row justify-between">
+                        <Link  href={`/history/${val.id}`} className="flex flex-row justify-between">
+                            <span>{val.user.name}</span>
+                            <span>{val.name}</span>
+                            <span>{val.amount}</span>
+                            <span>{val.status.name}</span> 
+                        </Link>
+                    </div>
+                )
+            })}
+            
+        </div>
+    )
 }
