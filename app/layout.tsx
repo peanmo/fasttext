b0fc2e85cb16885312552a7f5eeb52bf80fa9282
Armboy122 <arm.09686@gmail.com>
2024-06-22 11:45:48 +0700

    layout main beauty

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,17 +22,12 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="bg-pea min-w-full rounded-b-lg">
+        <nav className="bg-gradient-to-r from-pea to-purple-500 min-w-full rounded-b-lg">
           <div className="mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-16">
               <div className="flex items-center">
                 <div className="flex-shrink-0">
                   <Image src="/logo.png" alt="Logo" width={100} height={50} />
-                  <div className="flex-shrink-0">
-                    <a href="/" className="text-white">
-                      FAST TEXT
-                    </a>
-                  </div>
                 </div>
               </div>
               <div className="hidden md:block">
@@ -46,47 +41,63 @@ export default async function RootLayout({
                   {session &&
                     session.pea &&
                     ["admin", "checker"].includes(session.pea.role) && (
-                      <a
-                        href="/manage-docs"
-                        className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                      >
-                        จัดการเอกสารหลายฉบับ
-                      </a>
+                      <>
+                        <a
+                          href="/manage-docs"
+                          className="text-white hover:bg-white hover:text-black rounded-lg p-2"
+                        >
+                          จัดการเอกสารหลายฉบับ
+                        </a>
+                        <a
+                          href="/manage-doc"
+                          className="text-white hover:bg-white hover:text-black rounded-lg p-2"
+                        >
+                          จัดการเอกสาร
+                        </a>
+                      </>
                     )}
-                  {session &&
-                    session.pea &&
-                    ["admin", "checker"].includes(session.pea.role) && (
-                      <a
-                        href="/manage-doc"
-                        className="text-white hover:bg-white hover:text-black rounded-lg p-2"
+                  <div className="relative inline-block text-left group">
+                    <button className="text-white hover:bg-white hover:text-black rounded-lg p-2">
+                      เพิ่มเอกสาร
+                    </button>
+                    <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden group-hover:block">
+                      <div
+                        className="py-1"
+                        role="menu"
+                        aria-orientation="vertical"
+                        aria-labelledby="options-menu"
                       >
-                        จัดการเอกสาร
-                      </a>
-                    )}
-                  <a
-                    href="/form/create/benefit"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    สวัสดิการ
-                  </a>
-                  <a
-                    href="/form/create/procurement"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    จัดซื้อจัดจ้าง
-                  </a>
-                  <a
-                    href="/form/create/thousand"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    จัดซื้อจัดจ้างเกิน 1 แสน
-                  </a>
-                  <a
-                    href="/form/create/guarantee"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    คืนค่าประกัน
-                  </a>
+                        <a
+                          href="/form/create/benefit"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          role="menuitem"
+                        >
+                          สวัสดิการ
+                        </a>
+                        <a
+                          href="/form/create/procurement"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          role="menuitem"
+                        >
+                          จัดซื้อจัดจ้าง
+                        </a>
+                        <a
+                          href="/form/create/thousand"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          role="menuitem"
+                        >
+                          จัดซื้อจัดจ้างเกิน 1 แสน
+                        </a>
+                        <a
+                          href="/form/create/guarantee"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          role="menuitem"
+                        >
+                          คืนค่าประกัน
+                        </a>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="md:hidden">
@@ -111,7 +122,6 @@ export default async function RootLayout({
                       </svg>
                     </label>
                     <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden peer-checked:block">
-                      {/* peer-checked:block  cssที่ใช้ในการเปิดปิดการแสดงผล */}
                       <div
                         className="py-1"
                         role="menu"
@@ -128,51 +138,65 @@ export default async function RootLayout({
                         {session &&
                           session.pea &&
                           ["admin", "checker"].includes(session.pea.role) && (
-                            <a
-                              href="/manage-docs"
-                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                            >
-                              จัดการเอกสารหลายฉบับ
-                            </a>
+                            <>
+                              <a
+                                href="/manage-docs"
+                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                              >
+                                จัดการเอกสารหลายฉบับ
+                              </a>
+                              <a
+                                href="/manage-doc"
+                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                              >
+                                จัดการเอกสาร
+                              </a>
+                            </>
                           )}
-                        {session &&
-                          session.pea &&
-                          ["admin", "checker"].includes(session.pea.role) && (
+                        <div className="relative">
+                          <input
+                            type="checkbox"
+                            id="submenu-toggle"
+                            className="hidden peer"
+                          />
+                          <label
+                            htmlFor="submenu-toggle"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                            role="menuitem"
+                          >
+                            เพิ่มเอกสาร
+                          </label>
+                          <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden peer-checked:block">
                             <a
-                              href="/manage-doc"
+                              href="/form/create/benefit"
                               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                              role="menuitem"
                             >
-                              จัดการเอกสาร
+                              สวัสดิการ
                             </a>
-                          )}
-                        <a
-                          href="/form/benefit"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                          role="menuitem"
-                        >
-                          สวัสดิการ
-                        </a>
-                        <a
-                          href="/form/procurement"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                          role="menuitem"
-                        >
-                          จัดซื้อจัดจ้าง
-                        </a>
-                        <a
-                          href="/form/thousand"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                          role="menuitem"
-                        >
-                          จัดซื้อจัดจ้างเกิน 1 แสน
-                        </a>
-                        <a
-                          href="/form/guarantee"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                          role="menuitem"
-                        >
-                          คืนค่าประกัน
-                        </a>
+                            <a
+                              href="/form/create/procurement"
+                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                              role="menuitem"
+                            >
+                              จัดซื้อจัดจ้าง
+                            </a>
+                            <a
+                              href="/form/create/thousand"
+                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                              role="menuitem"
+                            >
+                              จัดซื้อจัดจ้างเกิน 1 แสน
+                            </a>
+                            <a
+                              href="/form/create/guarantee"
+                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                              role="menuitem"
+                            >
+                              คืนค่าประกัน
+                            </a>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import { authOptions } from "@/auth-options";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import Link from "next/link";
-import clientPromise from "@/lib/mongo";
+import clientpromise from "@/lib/mongo";
 import { ObjectId } from "mongodb";
 
 type TypeDocsCount = {
   waitingDocs: number;
   returnedDocs: number;
-  cancelDocs:number;
-  totalDocs:number
-}
+  cancelDocs: number;
+  totalDocs: number;
+};
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -18,48 +18,66 @@ export default async function Home() {
     redirect("/api/auth/signin");
   }
 
-  const typeDocsCount = await getTypeDocsCount(session.pea.id)
-  
+  const typeDocsCount = await getTypeDocsCount(session.pea.id);
+
   return (
-    <div className="flex flex-col gap-3 max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg">
+    <div className="flex flex-col gap-3 max-w-lg mx-auto p-6 bg-white rounded-lg shadow-lg">
       <h1 className="text-xl font-bold text-center">
         ยินดีต้อนรับ {session.pea.name}
       </h1>
-      <Link
-        href="/user/waiting-docs"
-        className="block p-4 bg-blue-500 text-white text-center rounded-lg shadow hover:bg-blue-600 transition duration-300"
-      >
-        รอเอกสารต้นฉบับ จำนวน {typeDocsCount.waitingDocs} ฉบับ
-      </Link>
-      <Link
-        href="/user/returned-docs"
-        className="block p-4 bg-green-500 text-white text-center rounded-lg shadow hover:bg-green-600 transition duration-300"
-      >
-        เอกสารส่งคืน/ตีกลับ จำนวน {typeDocsCount.returnedDocs} ฉบับ
-      </Link>
-      <Link
-        href="/user/cancel-docs"
-        className="block p-4 bg-red-500 text-white text-center rounded-lg shadow hover:bg-red-600 transition duration-300"
-      >
-        เอกสารถูกยกเลิก จำนวน {typeDocsCount.cancelDocs} ฉบับ
-      </Link>
-      <Link
-        href="/user/all-docs"
-        className="block p-4 bg-gray-500 text-white text-center rounded-lg shadow hover:bg-gray-600 transition duration-300"
-      >
-        เอกสารของคุณทั้งหมด จำนวน {typeDocsCount.totalDocs} ฉบับ
-      </Link>
+
+      <div className="flex items-center p-4 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-300">
+        <p className="text-4xl mr-4">📥</p>
+        <Link href="/user/waiting-docs">
+          <div className="text-left">
+            <h2 className="text-lg font-bold">รอเอกสารต้นฉบับ</h2>
+            <p>จำนวน {typeDocsCount.waitingDocs} ฉบับ</p>
+          </div>
+        </Link>
+      </div>
+
+      <div className="flex items-center p-4 bg-green-500 text-white rounded-lg shadow hover:bg-green-600 transition duration-300">
+        <p className="text-4xl mr-4">📤</p>
+        <Link href="/user/returned-docs">
+          <div className="text-left">
+            <h2 className="text-lg font-bold">เอกสารส่งคืน/ตีกลับ</h2>
+            <p>จำนวน {typeDocsCount.returnedDocs} ฉบับ</p>
+          </div>
+        </Link>
+      </div>
+
+      <div className="flex items-center p-4 bg-red-500 text-white rounded-lg shadow hover:bg-red-600 transition duration-300">
+        <p className="text-4xl mr-4">❌</p>
+        <Link href="/user/cancel-docs">
+          <div className="text-left">
+            <h2 className="text-lg font-bold">เอกสารถูกยกเลิก</h2>
+            <p>จำนวน {typeDocsCount.cancelDocs} ฉบับ</p>
+          </div>
+        </Link>
+      </div>
+
+      <div className="flex items-center p-4 bg-gray-500 text-white rounded-lg shadow hover:bg-gray-600 transition duration-300">
+        <p className="text-4xl mr-4">📂</p>
+        <Link href="/user/all-docs">
+          <div className="text-left">
+            <h2 className="text-lg font-bold">เอกสารของคุณทั้งหมด</h2>
+            <p>จำนวน {typeDocsCount.totalDocs} ฉบับ</p>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
 
-async function getTypeDocsCount(id:string) {
-  console.log(id)
-  try{
-    const mongoClient = await clientPromise
-    await mongoClient.connect()
-    const typeDocsCount = await mongoClient.db("nmo").collection("Status").aggregate(
-      [
+async function getTypeDocsCount(id: string) {
+  console.log(id);
+  try {
+    const mongoClient = await clientpromise;
+    await mongoClient.connect();
+    const typeDocsCount = (await mongoClient
+      .db("nmo")
+      .collection("Status")
+      .aggregate([
         {
           $group: {
             _id: "$documentId",
@@ -89,10 +107,9 @@ async function getTypeDocsCount(id:string) {
           },
         },
         {
-          $match:
-            {
-              "result.userId": new ObjectId(id),
-            },
+          $match: {
+            "result.userId": new ObjectId(id),
+          },
         },
         {
           $facet: {
@@ -142,94 +159,92 @@ async function getTypeDocsCount(id:string) {
           },
         },
         {
-          $project:
-            {
-              waitingDocs: {
-                $cond: {
-                  if: {
-                    $eq: [
-                      {
-                        $size: "$waitingDocs",
-                      },
-                      0,
-                    ],
-                  },
-                  then: 0,
-                  else: {
-                    $sum: "$waitingDocs.count",
-                  },
+          $project: {
+            waitingDocs: {
+              $cond: {
+                if: {
+                  $eq: [
+                    {
+                      $size: "$waitingDocs",
+                    },
+                    0,
+                  ],
                 },
-              },
-              returnedDocs: {
-                $cond: {
-                  if: {
-                    $eq: [
-                      {
-                        $size: "$returnedDocs",
-                      },
-                      0,
-                    ],
-                  },
-                  then: 0,
-                  else: {
-                    $sum: "$returnedDocs.count",
-                  },
-                },
-              },
-              cancelDocs: {
-                $cond: {
-                  if: {
-                    $eq: [
-                      {
-                        $size: "$cancelDocs",
-                      },
-                      0,
-                    ],
-                  },
-                  then: 0,
-                  else: {
-                    $sum: "$cancelDocs.count",
-                  },
-                },
-              },
-              totalDocs: {
-                $cond: {
-                  if: {
-                    $eq: [
-                      {
-                        $size: "$totalDocs",
-                      },
-                      0,
-                    ],
-                  },
-                  then: 0,
-                  else: {
-                    $sum: "$totalDocs.count",
-                  },
+                then: 0,
+                else: {
+                  $sum: "$waitingDocs.count",
                 },
               },
             },
+            returnedDocs: {
+              $cond: {
+                if: {
+                  $eq: [
+                    {
+                      $size: "$returnedDocs",
+                    },
+                    0,
+                  ],
+                },
+                then: 0,
+                else: {
+                  $sum: "$returnedDocs.count",
+                },
+              },
+            },
+            cancelDocs: {
+              $cond: {
+                if: {
+                  $eq: [
+                    {
+                      $size: "$cancelDocs",
+                    },
+                    0,
+                  ],
+                },
+                then: 0,
+                else: {
+                  $sum: "$cancelDocs.count",
+                },
+              },
+            },
+            totalDocs: {
+              $cond: {
+                if: {
+                  $eq: [
+                    {
+                      $size: "$totalDocs",
+                    },
+                    0,
+                  ],
+                },
+                then: 0,
+                else: {
+                  $sum: "$totalDocs.count",
+                },
+              },
+            },
+          },
         },
-      ]
-    ).toArray() as unknown as TypeDocsCount[]
-    await mongoClient.close()
-    if(typeDocsCount.length == 0){
+      ])
+      .toArray()) as unknown as TypeDocsCount[];
+    await mongoClient.close();
+    if (typeDocsCount.length == 0) {
       return {
         waitingDocs: 0,
         returnedDocs: 0,
-        cancelDocs:0,
-        totalDocs:0
-      }
+        cancelDocs: 0,
+        totalDocs: 0,
+      };
     }
-    return typeDocsCount[0]
-    
-  }catch(e){
-    console.log(e)
+    return typeDocsCount[0];
+  } catch (e) {
+    console.log(e);
     return {
       waitingDocs: 0,
       returnedDocs: 0,
-      cancelDocs:0,
-      totalDocs:0
-    }
+      cancelDocs: 0,
+      totalDocs: 0,
+    };
   }
 }
